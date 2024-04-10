0be98d0dfd001d955fd26ea2d32c4b2d8cf0be42
Jakub Śliwka <kubapplll@gmail.com>
2024-04-10 22:50:00 +0200

    only ex 5 left. Noice

--- js_l/list_4/analise_file.cpp
+++ js_l/list_4/analise_file.cpp
@@ -10,6 +10,8 @@ int run(string file_name);
 int read_from_file(string* file_name);
 int count_signs(vector <string>* lines);
 int count_words(vector <string>* lines);
+string find_most_freq_word(vector <string>* lines);
+string find_most_freq_sign(vector <string>* lines);
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
@@ -45,8 +47,8 @@ int read_from_file(string* file_name) {
     cout << 2 << "," << count_signs(&lines)<<"\n";    
     cout << 3 << "," << count_words(&lines)<<"\n";
     cout << 4 << "," << lines.size()-1<<"\n";
-    // cout << 5 << "," << lines.size()<<"\n";
-    // cout << 6 << "," << lines.size()<<"\n";
+    cout << 5 << "," << find_most_freq_sign(&lines) <<"\n";
+    cout << 6 << "," << find_most_freq_word(&lines) <<"\n";
     return 0;
 }
 
@@ -80,4 +82,66 @@ int count_words(vector <string>* lines){
         }
     }
     return count;
+}
+
+string find_most_freq_word(vector <string>* lines){
+    string all_file = "";
+    for (int i = 0; i < lines->size(); i++) {
+        all_file += lines->at(i);
+        all_file += " ";
+    }
+    vector<string> words;
+    for (int i = 0; i < all_file.size(); i++) {
+        string word = "";
+        while (all_file[i] != ' ' && all_file[i] != '\n'){
+            word += all_file[i];
+            i++;
+        }
+        words.push_back(word);
+    }
+
+    string most_freq_word = "";
+    int most_freq_word_count = 0;
+    for (int i = 0; i < words.size(); i++) {
+        if (words[i] != most_freq_word){
+            int tmp = 0;
+            for (int j = 0; j < words.size(); j++) {
+                if (words[i] == words[j]){
+                    tmp++;
+                }
+            }   
+            if(tmp > most_freq_word_count){
+                most_freq_word = words[i];
+                most_freq_word_count = tmp;
+            }
+        }
+    }
+        
+    
+    return most_freq_word;
+}
+
+string find_most_freq_sign(vector <string>* lines){
+    string all_file = "";
+    for (int i = 0; i < lines->size(); i++) {
+        all_file += lines->at(i);
+    }
+
+    string most_freq_sign = "";
+    int most_freq_sign_count = 0;
+    for (int i = 0; i < all_file.size(); i++) {
+        if (all_file[i] != most_freq_sign[0]){
+                int tmp = 0;
+                for (int j = 0; j < all_file.size(); j++) {
+                    if (all_file[i] == all_file[j]){
+                        tmp++;
+                    }
+                }   
+                if(tmp > most_freq_sign_count){
+                    most_freq_sign = all_file[i];
+                    most_freq_sign_count = tmp;
+                }
+            }
+    }
+    return most_freq_sign;
 }
--- js_l/list_4/read_files.py
+++ js_l/list_4/read_files.py
@@ -1,34 +1,70 @@
 import subprocess,sys,os, logging, csv
+from typing import Optional
 
-logger = logging.getLogger(__name__)
+# logging.basicConfig(
+#      format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+#      datefmt='%H:%M:%S'
+#  )
 
-def get_path() -> str:
-    return sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+def find_most_freq(list_of_words: list[str]) -> str:
+    counter = 0
+    num = list_of_words[0]
+     
+    for i in list_of_words:
+        curr_frequency = list_of_words.count(i)
+        if(curr_frequency> counter):
+            counter = curr_frequency
+            num = i
+ 
+    return num
+
+def get_path() -> Optional[str]:
+    return sys.argv[1] if len(sys.argv) > 1 else None
 
 def read_files( path:str ) -> None:
-    pass
-
-def run():
-    path = get_path()
-    if os.path.exists(path):
-        read_files(path)
-    else:
-        logger.error(f"Path {path} does not exist")
-
-def run():
     sum_read_files = 0
     sum_chars = 0
     sum_words = 0
     sum_lines = 0
-    most_common_char = ""
-    most_common_word = ""
-    
-    # print(get_path())
-    
-    x = subprocess.run(["./analise_file", "tmp.txt"],  encoding='ascii', capture_output=True)
-    y = csv.reader(x.stdout.splitlines())
-    for y in y:
-        print(y)    
+    most_common_char = []
+    most_common_word = []
+     
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+
+            output = subprocess.run(["./analise_file", os.path.join(path, file)],  encoding='ascii', capture_output=True)
+            csv_output = csv.reader(output.stdout.splitlines())
+            for row in csv_output:
+                match row[0]:
+                    case "1":
+                        sum_read_files += 1
+                    case "2":
+                        sum_chars += int(row[1])
+                    case "3":
+                        sum_words += int(row[1])
+                    case "4":
+                        sum_lines += int(row[1])
+                    case "5":
+                        most_common_char.append(row[1])
+                    case "6":
+                        most_common_word.append(row[1])
+    print (f"Number of files read: {sum_read_files}")
+    print (f"Number of characters: {sum_chars}")
+    print (f"Number of words: {sum_words}")
+    print (f"Number of lines: {sum_lines}")
+    print (f"Most common character: '{find_most_freq(most_common_char)}'")
+    print (f"Most common word: {find_most_freq(most_common_word)}")
+
+def run():
+    path = get_path()
+    if path:    
+        if os.path.exists(path):
+            read_files(path)
+        else:
+            logging.error(f"Path {path} does not exist")
+    else:
+        logging.error("No path provided")
+         
 
 if __name__ == '__main__':
     run()
